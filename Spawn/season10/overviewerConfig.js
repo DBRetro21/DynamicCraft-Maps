83fe011bbe54499c2edf6f4e54df348c96c301ee
DB_Retro <125267441+DBRetro21@users.noreply.github.com>
2024-02-09 18:00:15 +0000

    Initial Upload
    The Beginning of the server's map project.

---
+++ Spawn/season10/overviewerConfig.js
@@ -0,0 +1,72 @@
+var overviewerConfig = {
+    "CONST": {
+        "tileSize": 384,
+        "image": {
+            "defaultMarker": "signpost.png",
+            "signMarker": "signpost_icon.png",
+            "bedMarker": "bed.png",
+            "spawnMarker": "markers/marker_home.png",
+            "spawnMarker2x": "markers/marker_home_2x.png",
+            "queryMarker": "markers/marker_location.png",
+            "queryMarker2x": "markers/marker_location_2x.png",
+            "compass": {
+                "0": "logo.png",
+                "1": "logo.png",
+                "3": "logo.png",
+                "2": "logo.png"
+            }
+        },
+        "mapDivId": "mcmap",
+        "UPPERLEFT": 0,
+        "UPPERRIGHT": 1,
+        "LOWERLEFT": 3,
+        "LOWERRIGHT": 2
+    },
+    "worlds": [
+        "Spawn"
+    ],
+    "map": {
+        "debug": false,
+        "cacheTag": "1704485532",
+        "north_direction": "lower-left",
+        "controls": {
+            "pan": true,
+            "zoom": true,
+            "spawn": true,
+            "compass": true,
+            "mapType": true,
+            "overlays": true,
+            "coordsBox": true
+        }
+    },
+    "tilesets": [
+        {
+            "name": "Season 10",
+            "zoomLevels": 7,
+            "defaultZoom": 1,
+            "maxZoom": 7,
+            "path": "DB_Retro",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Spawn",
+            "last_rendertime": 1704484430,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                0,
+                50,
+                0
+            ],
+            "lastrenderversion": 2,
+            "minZoom": 0,
+            "spawn": [
+                0,
+                50,
+                0
+            ],
+            "north_direction": 1
+        }
+    ]
+};
---
+++ Spawn/season9/overviewerConfig.js
@@ -0,0 +1,72 @@
+var overviewerConfig = {
+    "CONST": {
+        "tileSize": 384,
+        "image": {
+            "defaultMarker": "signpost.png",
+            "signMarker": "signpost_icon.png",
+            "bedMarker": "bed.png",
+            "spawnMarker": "markers/marker_home.png",
+            "spawnMarker2x": "markers/marker_home_2x.png",
+            "queryMarker": "markers/marker_location.png",
+            "queryMarker2x": "markers/marker_location_2x.png",
+            "compass": {
+                "0": "logo.png",
+                "1": "logo.png",
+                "3": "logo.png",
+                "2": "logo.png"
+            }
+        },
+        "mapDivId": "mcmap",
+        "UPPERLEFT": 0,
+        "UPPERRIGHT": 1,
+        "LOWERLEFT": 3,
+        "LOWERRIGHT": 2
+    },
+    "worlds": [
+        "Spawn"
+    ],
+    "map": {
+        "debug": false,
+        "cacheTag": "1705647908",
+        "north_direction": "lower-left",
+        "controls": {
+            "pan": true,
+            "zoom": true,
+            "spawn": true,
+            "compass": true,
+            "mapType": true,
+            "overlays": true,
+            "coordsBox": true
+        }
+    },
+    "tilesets": [
+        {
+            "name": "Season 9",
+            "zoomLevels": 10,
+            "defaultZoom": 1,
+            "maxZoom": 10,
+            "path": "DB_Retro",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Spawn",
+            "last_rendertime": 1703584880,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                0,
+                63,
+                -5
+            ],
+            "lastrenderversion": 2,
+            "minZoom": 0,
+            "spawn": [
+                0,
+                63,
+                -5
+            ],
+            "north_direction": 1
+        }
+    ]
+};
